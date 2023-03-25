37af1779ef23c49ea7ab23284416dd085ce6cdce
Imon <contact@imonweb.co.uk>
2023-03-25 00:56:19 +0000

    simple maps

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ const Section = styled.div`
   scroll-snap-align: center;
 `
 const Container = styled.div`
-  width: 100px;
+  width: 100%;
   height: 100%;
   display: flex;
   justify-content: space-between;
@@ -14,21 +14,38 @@ const Container = styled.div`
 `
 const Left = styled.div`
   flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+ 
 `
 const Title = styled.h1`
-  
+  font-weight: 200;
 `
 const Form = styled.form`
-  
+  width: 500px;
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
 `
 const Input = styled.input`
-  
+  padding: 20px;
+  background-color: #e8e6e6;
+  border: none;
+  border-radius: 5px;
+
 `
 const TextArea = styled.textarea`
-  
+  padding: 20px;
 `
 const Button = styled.button`
-  
+  background-color: #da4ea2;
+  color: #fff;
+  border: none;
+  font-weight: bold;
+  cursor: pointer;
+  border-radius: 5px;
+  padding: 20px;
 `
 const Right = styled.div`
   flex: 1;
@@ -45,7 +62,11 @@ function Contact() {
             <Title>Contact Us</Title>
             <Input placeholder="Name" /> 
             <Input placeholder="Email" /> 
-            <TextArea placeholder="Write your message" /> 
+            <TextArea 
+              placeholder="Write your message" 
+              name="message"
+              rows={10}
+            /> 
             <Button>Send</Button>
           </Form>
         </Left>

dc79fc0fafe70f5ab75b64dc7c82322075ed99ce
Imon <contact@imonweb.co.uk>
2023-03-28 22:22:39 +0100

    test

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,8 +1,22 @@
+import { Canvas } from '@react-three/fiber'
 import React from 'react'
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  scroll-snap-align: center;
+`
 
 const Test = () => {
   return (
-    <div>Test</div>
+    <Container>
+      <Canvas>
+        <mesh>
+          <boxGeometry args={[1,1,1]}/>
+        </mesh>
+      </Canvas>
+    </Container>
   )
 }
 

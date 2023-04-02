7a3f13ea5ba47fdb85607721176b37ec0cefb1cb
Imon <contact@imonweb.co.uk>
2023-04-02 22:24:07 +0100

    hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -92,11 +92,18 @@ const Hero = () => {
           <Button>Learn More</Button>
         </Left>
         <Right>
-           <Canvas camera={{fov:25, position:[5,5,5]}}>
+           <Canvas>
             <OrbitControls enableZoom={false}/> 
             <ambientLight intensity={1} />
             <directionalLight position={[3,2,1]} />
-            <Sphere args={[1,100,200]} scale={1} />
+            <Sphere args={[1,100,200]} scale={1.1}>
+            <MeshDistortMaterial 
+              color="#3d1c56"
+              attach="material"
+              distort={0.5}
+              speed={2}
+              />
+              </Sphere>
           </Canvas>
           <Img src="./img/moon.png" /> 
         </Right>
